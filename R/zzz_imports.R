fbd8a6f1e3eca23cd40fc0c5e7723e81e9c55d02
pbrve <pbrve@dtu.dk>
2025-01-24 14:37:08 +0100

    trying to make sure gsl is linked to on all systems - changing makevars files and importing RcppGSL

--- R/zzz_imports.R
+++ R/zzz_imports.R
@@ -1,5 +1,8 @@
 #' @importFrom utils head
 #' @importFrom stats pnorm acf as.formula cov2cor density lag na.pass nlminb pacf pnorm pt
 #' @importFrom stats frequency fft spec.taper
-#' @import R6 Rcpp RcppEigen RcppZiggurat
+#' @import R6
+#' @rawNamespace import(Rcpp, except=LdFlags)
+#' @rawNamespace import(RcppEigen, except=c(fastLmPure, fastLm))
+#' @import RcppZiggurat RcppGSL
 NULL
