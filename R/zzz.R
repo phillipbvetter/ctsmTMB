fbb9bf86ad463d80b0956665e15657c3c2ae09cd
pbrve <pbrve@dtu.dk>
2025-04-02 13:49:51 +0200

    using packageStartupMessage instead of message

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
   # Check if RTMBode is installed:
   check <- requireNamespace("RTMBode",quietly=TRUE)
   if(!check){
-    message(
+    packageStartupMessage(
       "Note: The RTMBode package for adaptive ODE-solvers is not installed.
 The package can be installed from source using
   install.packages('RTMBode', repos = c('https://kaskr.r-universe.dev', 'https://cloud.r-project.org'))
