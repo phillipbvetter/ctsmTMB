8a64117700d733a99282c7ff5a7f857e78fcd943
Phillip Brinck Vetter <phillipvetter@outlook.com>
2026-03-23 15:04:19 +0100

    Deleted Ornstein500

--- R/zzz_data.R
+++ R/zzz_data.R
@@ -28,31 +28,6 @@
 #' @keywords data
 "Ornstein"
 
-#' Sample from a simulated Ornstein-Uhlenbeck process with time-dependent mean
-#' 
-#' @description
-#' The data was simulated using a standard Euler-Maruyama method. 
-#' 
-#' The simulated process is governed by the SDE
-#' #' dx ~ theta * (mu + u - x) * dt + sigma_x * dw
-#' 
-#' The parameters used for simulation were
-#' theta = 10, mu = 1, sigma_x = 1, sigma_y = 0.1
-#' 
-#' The simulation time-step was 1e-3, and observation time-step 1e-2. 
-#' The simulation was taken from t = 0..5 seconds
-#' 
-#' @format
-#' A data frame of 501 rows and 3 columns. The columns represent the 
-#' variables: `t` (time), `y` (observation) and `u` (input).
-#' 
-#' @usage Ornstein500
-#' 
-#' @name Ornstein500
-#' @docType data
-#' @keywords data
-"Ornstein500"
-
 #' Sample from a simulated two-state Ornstein-Uhlenbeck process
 #'
 #' @description
--- data-raw/Ornstein.R
+++ data-raw/Ornstein.R
@@ -1,45 +1,53 @@
 ## code to prepare `Ornstein` dataset goes here:
 
-############################################################
-# Data simulation
-############################################################
+# Create model object
+model <- newModel()
+model$addSystem(dx ~ theta * (mu-x+u) * dt + sigma_x*dw)
+model$addObs(y ~ x)
+model$setVariance(y ~ sigma_y^2)
+model$addInput(u)
+model$setParameter(
+  theta   = c(initial=2, lower=1e-5, upper=50),
+  mu      = c(initial=2, lower=0, upper=5),
+  sigma_x = c(initial=1e-2, lower=1e-10, upper=30),
+  sigma_y = c(initial=1e-2, lower=1e-10, upper=30)
+)
+model$setInitialState(list(x0=3, p0=0.01 * diag(1)))
 
-# Simulate data using Euler Maruyama
-set.seed(20)
+# Create initial data
 pars = c(theta=5, mu=3, sigma_x=1, sigma_y=0.1)
-# 
 dt.sim = 1e-3
 t.sim = seq(0,20,by=dt.sim)
 dw = rnorm(length(t.sim)-1,sd=sqrt(dt.sim))
 u.sim = cumsum(rnorm(length(t.sim),sd=0.05))
-x = 3
-for(i in 1:(length(t.sim)-1)) {
-  x[i+1] = x[i] + pars[1]*(pars[2]-x[i]+u.sim[i])*dt.sim + pars[3]*dw[i]
-}
+df.sim <- data.frame(
+  t = t.sim,
+  u = u.sim,
+  y = NA 
+)
 
-# Extract observations and add noise
+# Simulate
+cpp.seeds <- c(20,20)
+sim <- model$simulate(data=df.sim,
+                      pars = pars,
+                      method="ekf", 
+                      n.sims = 1,
+                      cpp.seeds = cpp.seeds)
+y.sim <- sim$observations$y$i0[,1]
+
+# Extract observed data
 dt.obs = 1e-1
 ids = seq(1,length(t.sim),by=round(dt.obs / dt.sim))
-t.obs = t.sim[ids]
-y = x[ids] + pars[4] * rnorm(length(t.obs))
-# forcing input
-u = u.sim[ids]
-
 # Create data
-.data = data.frame(
-  t = t.obs,
-  y = y,
-  u = u
+Ornstein = data.frame(
+  t = t.sim[ids],
+  y = y.sim[ids],
+  u = u.sim[ids]
 )
 
-# create data
-Ornstein = data.frame(
-  t = t.obs,
-  y = y,
-  u = u
-)
 
 # plot(Ornstein$t,Ornstein$y,type="l")
 
 # uncomment below to update
+# usethis::proj_set("~/github/ctsmTMB")
 # usethis::use_data(Ornstein, overwrite = TRUE)
